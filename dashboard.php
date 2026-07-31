b8dd69b1b91301b9718b35486291e1dfd6abff0e
Abdelwahab <abdelwahab.ahmed60@gmail.com>
2026-07-31 03:46:11 +0300

    Refactor database connection logic, streamline logout process, and improve code consistency

--- dashboard.php
+++ dashboard.php
@@ -1,32 +1,7 @@
 <?php
 session_start();
 
-if (empty($_SESSION['user_id'])) { header('Location: index.php'); exit; }
-
-$userName = $_SESSION["user_firstName"] . " " . $_SESSION["user_lastName"];
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
-
-    $_SESSION = [];
-
-    if (ini_get('session.use_cookies')) {
-        $params = session_get_cookie_params();
-        setcookie(
-            session_name(),
-            '',
-            time() - 60,
-            $params['path'],
-            $params['domain'],
-            $params['secure'],
-            $params['httponly']
-        );
-    }
-
-    session_destroy();
-    header("Location: http://localhost/projects/registerForm/index.php");
-    exit;
-}
+require_once "logout.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
